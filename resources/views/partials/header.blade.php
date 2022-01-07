e9d7b586082272879eebfc52f0991e82a798cada
farhannfdl <farhan972015@gmail.com>
2022-01-07 16:26:20 +0700

    7 januari 2022

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,9 +2,9 @@
 <div class="container">
         <div class="row my-auto" id="headers">
                 <div class="col-lg-4 col-md-6 col-sm-12">
-                        <img src="{{ asset('images/logo_hmif.png') }}" class="img-logo mx-auto">
+                        <img src="{{ asset('images/logo_hmif.png') }}" class="img-logo">
                 </div>
-                <div class="col-lg-6 col-md-6 col-sm-12">
+                <div class="col-lg-6 col-md-6 col-sm-12 header-desc">
                         <h1>Himpunan Mahasiswa Informatika</h1>
                         <p>Institut Teknologi Indonesia</p>
                         <button type="button" class="btn btn-primary btn-header">More About Us</button>
